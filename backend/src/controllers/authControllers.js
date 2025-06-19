53eed4fa0f316db40e415afc6abc59ec25a0be7f
Arteaga08 <arteaga_manuel88@outlook.com>
2025-06-19 13:18:18 -0600

    signup created

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,9 +1,14 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/userModel.js";
 import bycrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
     if (password.length < 6) {
       return res
         .status(400)
@@ -22,7 +27,24 @@ export const signup = async (req, res) => {
       email,
       password: hashedPassword,
     });
-  } catch (error) {}
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "invalid user" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const login = (req, res) => {
