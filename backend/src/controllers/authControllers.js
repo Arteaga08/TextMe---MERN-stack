e1948a270bff83943f720ad0e6ea72ddb2bee454
Arteaga08 <arteaga_manuel88@outlook.com>
2025-06-18 21:00:20 -0600

    jwt created

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,5 +1,28 @@
-export const signup = (req, res) => {
-  res.send("signup route");
+import User from "../models/userModel.js";
+import bycrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bycrypt.genSalt(10);
+    const hashedPassword = await bycrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
