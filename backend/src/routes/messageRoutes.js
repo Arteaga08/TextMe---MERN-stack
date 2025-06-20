33716623e673a790264bb10d74f15a502c9cdaa6
Arteaga08 <arteaga_manuel88@outlook.com>
2025-06-19 20:06:49 -0600

    getMessages function created

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
-import { getUsersForSidebar } from "../controllers/messageController";
-
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getUsersForSidebar } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
