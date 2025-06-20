df66f0c29b0c52acadeb6d5354f700a1dc63db28
Arteaga08 <arteaga_manuel88@outlook.com>
2025-06-19 19:41:28 -0600

    message Controller and Route created

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js"
 
 dotenv.config();
 const app = express();
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
