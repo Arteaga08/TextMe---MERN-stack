05c5f9bb1c0f7dd17abe670b4eef0ed07a61cb48
Arteaga08 <arteaga_manuel88@outlook.com>
2025-06-24 14:24:16 -0600

    Fixed error

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
