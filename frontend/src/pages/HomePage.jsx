94d44b9b2d99d245c015c73e115af6d6f43de107
Arteaga08 <arteaga_manuel88@outlook.com>
2025-06-26 12:12:41 -0600

    SideBar showing users

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,12 @@
 import { useChatStore } from "../store/useChatStore";
 
-
 import NoChatSelected from "../components/NoChatSelected";
 import SideBar from "../components/SideBar";
 import ChatContainer from "../components/ChatContainer";
 
- 
- 
- const HomePage = () => {
-  const {selectedUser} = useChatStore()
-   return (
+const HomePage = () => {
+  const { selectedUser } = useChatStore();
+  return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
@@ -22,7 +19,6 @@ import ChatContainer from "../components/ChatContainer";
       </div>
     </div>
   );
- }
- 
- export default HomePage
- 
+};
+
+export default HomePage;
