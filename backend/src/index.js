f71697eb9dca051c249c8325ea3104ba078009fd
Arteaga08 <arteaga_manuel88@outlook.com>
2025-06-27 21:10:20 -0600

    Deploy #3

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,10 +36,11 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+    app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
